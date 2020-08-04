60dd8f686db998ff4bf717e3e8e3dc58a7567858
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-04 11:01:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+ changeContent();
+});
+
+function changeContent(){
+  document.getElementById('text').innerHTML = "This is really cool!";
+}
